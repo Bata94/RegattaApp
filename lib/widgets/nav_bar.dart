cd52bf40ecc9d22fc9eeb575042d0f27b9684763
Bastian Sievers <b.sievers@sits.ruhr>
2024-07-01 09:54:06 +0200

    fixed NavBar!

--- lib/widgets/nav_bar.dart
+++ lib/widgets/nav_bar.dart
@@ -12,44 +12,45 @@ class NavBar extends AppBar {
   }) : super(
     key: Key("appBar-$title"),
     toolbarHeight: height,
-    flexibleSpace: AppBar(
-      backgroundColor: Theme.of(context).primaryColor,
-      titleTextStyle: Theme.of(context).textTheme.displaySmall!.copyWith(color: Colors.white),
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Image.asset(
-            "assets/icons/mrg_logo.png",
-            width: 32,
-            height: 32,
-          ),
-          Text(
-            title,
-          ),
-          const SizedBox(
-            width: 32,
-            height: 32,
-          ),
-        ],
-      ),
-      actions: [
-        IconButton(
-          icon: const Icon(Icons.logout),
-          onPressed: () async {
-            bool success = await Provider.of<AuthProvider>(context, listen: false).logout();
-
-            if (success) {
-              // TODO: fix this!
-
-              // ignore: use_build_context_synchronously
-              Navigator.popUntil(context, ModalRoute.withName('/home'));
-              await Future.delayed(const Duration(milliseconds: 10));
-              // ignore: use_build_context_synchronously
-              Navigator.of(context).pushReplacementNamed('/login');
-            }
-          },
+    iconTheme: Theme.of(context).iconTheme.copyWith(color: Theme.of(context).colorScheme.onPrimary),
+    backgroundColor: Theme.of(context).primaryColor,
+    titleTextStyle: Theme.of(context).textTheme.displaySmall!.copyWith(color: Theme.of(context).colorScheme.onPrimary),
+    title: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Image.asset(
+          "assets/icons/mrg_logo.png",
+          width: 32,
+          height: 32,
+        ),
+        Text(
+          title,
+        ),
+        const SizedBox(
+          width: 32,
+          height: 32,
         ),
       ],
     ),
+    actions: [
+      IconButton(
+        icon: const Icon(
+          Icons.logout,
+        ),
+        onPressed: () async {
+          bool success = await Provider.of<AuthProvider>(context, listen: false).logout();
+
+          if (success) {
+            // TODO: fix this!
+
+            // ignore: use_build_context_synchronously
+            Navigator.popUntil(context, ModalRoute.withName('/home'));
+            await Future.delayed(const Duration(milliseconds: 10));
+            // ignore: use_build_context_synchronously
+            Navigator.of(context).pushReplacementNamed('/login');
+          }
+        },
+      ),
+    ],
   );
 }
